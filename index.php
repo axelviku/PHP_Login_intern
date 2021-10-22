8d46edefcf48d2843bb9d2eb9dde5a035b564854
axelice <axelice89@gmail.com>
2021-10-22 17:22:38 +0530

    frontend part alert seprate in alert.php

--- index.php
+++ index.php
@@ -21,7 +21,6 @@
                 }  
              }else{
                  $valid_phn_num = "Enter Valid Phone Num!!!";
-                 //echo "Enter Valid Phone Number";
              }
             } else {  
                 $email_exist = "Email Already Exist!!!";  
@@ -31,54 +30,8 @@
         }  
     }  
 ?>  
-<?php
- if($reg_success){
-  echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
-  <strong>Success!</strong> ' . $reg_success . '
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-      <span aria-hidden="true">&times;</span>
-  </button>
-</div>';
- }
-?>
-<?php
- if($reg_not_success){
-  echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-  <strong>Error!</strong> ' . $reg_not_success . '
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-      <span aria-hidden="true">&times;</span>
-  </button>
-</div>';
- }
-
- if($email_exist){
-    echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-    <strong>Error!</strong> ' . $email_exist . '
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
-  </div>';
-   }
-
-   if($pass_match){
-    echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-    <strong>Error!</strong> ' . $pass_match . '
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
-  </div>';
-   }
-
-   if($valid_phn_num){
-    echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-    <strong>Error!</strong> ' . $valid_phn_num . '
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
-  </div>';
-   }
-
-?>
+<!-- this include the alert message shown on the registration page  -->
+<?Php include 'alert.php';?>
 
 <!DOCTYPE html>  
  <html lang="en" class="no-js">  
